5fd695705096e386525610386e6182a2cc26e9a1
Projetndagano <ndaganoprojet@gmail.com>
2024-09-22 14:32:06 +0300

    Add files via upload

---
+++ sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+//note that i have divided automatically those fractions to simplify the task
+	double a=0.333;// here i am defining my variables
+	double b=0.6;
+	double c=0.71428571;
+	double d=0.77777778;
+	double e=0.97938144;
+	double sum=a+b+c+d+e;// let us sum these fractions
+	std::cout<<"The sum of the the series is:  "<<sum;//here we should see now the sum of those fractions
+	return 0;}
+	
+
